29aa8e052aa99a17c5783b2eff0cc9bd1f81b32b
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-06-24 23:38:31 +0800

    Add Livewire components for patient management: CreatePage, Table, and UpdatePage

---
+++ app/Livewire/Patients/Table.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Patients;
+
+use Livewire\Component;
+
+class Table extends Component
+{
+    public function render()
+    {
+        return view('livewire.patients.table');
+    }
+}
