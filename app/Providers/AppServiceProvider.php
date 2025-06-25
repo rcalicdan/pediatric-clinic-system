6bd8f9bed2bf6f612ccba11450bd4f9a7a4d7319
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-06-25 10:41:55 +0800

    feat: Register AppointmentObserver for automatic appointment event handling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Appointment;
+use App\Observers\AppointmentObserver;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::automaticallyEagerLoadRelationships();
+
+        Appointment::observe(AppointmentObserver::class);
     }
 }
